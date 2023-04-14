6e888747ef3136b4877f8b93c1140617a2f481bc
Steven Price <srprice@umich.edu>
2023-04-14 00:00:14 -0400

    Update testSelectionSort.java

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,5 +1,7 @@
 package lab10Original;
 
+import org.junit.jupiter.api.Test;
+
 public class testSelectionSort {
 	@Test
 	public void test() {
@@ -9,9 +11,7 @@ public class testSelectionSort {
 		testDuplicates();
 	}
 
-    public testSelectionSort() {
-    }
-    
+    @Test
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -28,11 +28,30 @@ public class testSelectionSort {
         Sortedarr[4] = 10;
         
         /** add tests to check for this unit test **/
+        for(int i = 0; i < Sortedarr.length; i++) {
+        	assert(Sortedarr[i]>0);
+        }
     }
-    
+    @Test
     public void testNegative(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
         
-        /** Test data contains negative values only **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -2;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -9;
+        Sortedarr[4] = -10;
+        
+        /** add tests to check for this unit test **/
+        for(int i = 0; i < Sortedarr.length; i++) {
+        	assert(Sortedarr[i]<0);
+        }
     }
     
     public void testMixed(){
